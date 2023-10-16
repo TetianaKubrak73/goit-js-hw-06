5d8df689516242dbb6a822a46989404079c0fe67
TetianaKubrak73 <tetianakubrak73@gmail.com>
2023-10-16 21:23:39 +0200

    task 1-3

---
+++ src/src/js/task-01.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const categoriesList = document.getElementById('categories');
+  const categoryItems = categoriesList.querySelectorAll('.item');
+
+  console.log(`Number of categories: ${categoryItems.length}`);
+
+  categoryItems.forEach((item) => {
+    const categoryTitle = item.querySelector('h2').textContent;
+    const categoryElements = item.querySelectorAll('li').length;
+    console.log(`Category: ${categoryTitle}`);
+    console.log(`Elements: ${categoryElements}`);
+  });
+});
